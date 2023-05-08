45af32fa593f451892577cb54c088eefcbe816f9
chihiro <zeny10226@gmail.com>
2023-05-08 09:49:41 +0900

    최초 등록

---
+++ src/main/java/com/study/springboot/Teamp1Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teamp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/springboot/Teamp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teamp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
